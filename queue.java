bbcd22fedd3e70bf0a51a6c3c4130ab04ed98345
Rohan K Joseph <rohankj77@gmail.com>
2025-10-28 22:34:34 +0530

    queue

---
+++ queue.java
@@ -0,0 +1,32 @@
+import java.util.Queue;
+
+public class queue {
+    public static void main(String[] args) {
+        Queue<String> queue = new java.util.LinkedList<String>();
+
+        // Adding elements to the queue
+        queue.add("alkmvl");
+        queue.add("bvmflkm");
+        queue.add("cmlvml");
+
+        System.out.println("Queue after adding elements: " + queue);
+        
+        // Removing an element from the queue
+        String removedElement = queue.remove();
+        System.out.println("Removed element: " + removedElement);
+        System.out.println("Queue after removing an element: " + queue);
+
+        // Peeking at the front element of the queue
+        String frontElement = queue.peek();
+        System.out.println("Front element: " + frontElement);
+        System.out.println("Queue after peeking: " + queue);
+
+        // Checking if the queue is empty
+        boolean isEmpty = queue.isEmpty();
+        System.out.println("Is the queue empty? " + isEmpty);
+
+        // Getting the size of the queue
+        int size = queue.size();
+        System.out.println("Size of the queue: " + size);
+    }
+}
